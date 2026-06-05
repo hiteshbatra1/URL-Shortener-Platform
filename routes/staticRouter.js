b9cbbbc61592d970d05aee8be32debd7a4ff7f8f
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-06-05 19:47:09 +0530

    Add static router for handling homepage, signup, and login routes

---
+++ routes/staticRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const URL = require("../models/url");
+
+router.get("/", async (req, res) => {
+  const allUrls = await URL.find({});
+  return res.render("index", {
+    urls: allUrls,
+  });
+});
+
+router.get("/signup", (req, res) => {
+  return res.render("signup");
+});
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+module.exports = router;
