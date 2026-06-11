0f7c22972263706385a81ec9f0e2942b74116f42
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-06-11 22:54:59 +0530

    Add URL routes for handling short URL generation, redirection, and analytics

---
+++ routes/url.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const {
+  handleGenerateNewShortUrl,
+  handleRedirectToUrl,
+  handleGetAnalytics,
+} = require("../controllers/url");
+
+router.post("/", handleGenerateNewShortUrl);
+
+router.get("/:shortId", handleRedirectToUrl);
+
+router.get("/analytics/:shortId", handleGetAnalytics);
+
+module.exports = router;
